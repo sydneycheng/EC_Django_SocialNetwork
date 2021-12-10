a484a4579cb078d1e30ba4115daadb9c760f4719
sydneycheng <sydney_cheng1@baylor.edu>
2021-12-10 13:11:00 -0600

    Procfile

--- FeedProject/settings.py
+++ FeedProject/settings.py
@@ -132,3 +132,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'users:login'
+
+# Heroku settings
+import django_heroku
+django_heroku.settings(locals())
