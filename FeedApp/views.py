e727a7688dff0a643b9d890c8b1f5b221680a491
sydneycheng <sydney_cheng1@baylor.edu>
2021-12-10 02:52:51 -0600

    new_post

--- FeedApp/views.py
+++ FeedApp/views.py
@@ -42,14 +42,36 @@ def myfeed(request):
     like_count_list = []
     posts = Post.objects.filter(username=request.user).order_by('-date_posted')
     for p in posts:
-        c_count = Comment.objects.filter(posts=p).count()
-        l_count = Like.objects.filter(posts=p).count()
-        comment_count_list.append(l_count)
+        c_count = Comment.objects.filter(post=p).count()
+        l_count = Like.objects.filter(post=p).count()
+        comment_count_list.append(c_count)
+        like_count_list.append(l_count)
     zipped_list = zip(posts,comment_count_list,like_count_list)
 
     context = {'posts':posts, 'zipped_list':zipped_list}
     return render(request, 'FeedApp/myfeed.html', context)
 
+
+@login_required
+def new_post(request):
+    if request.method != "POST":
+        form = PostForm()
+    else:
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.username = request.user
+            new_post.save()
+            return redirect('FeedApp:myfeed')
+
+    context = {'form':form}
+    return render(request, 'FeedApp/new_post.html', context)
+
+@login_required
+def comments(request,post_id):
+    if request.method == 'POST' and request.POST.get("btn1"):
+        comment = request.POST.get("comment")
+        Comment.objects.create(post_id=post_id,username=request.user,text=comment,date_added=date.today())
 
 
 
